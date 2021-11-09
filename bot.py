9a5f19207fb7d7bc84ace84d432e37b65789eac0
rhys <87879237+rhysfr@users.noreply.github.com>
2021-11-09 09:48:10 -0800

    Creating Bot.py
    Base Code

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord # importing discord module
+
+bot = discord.Client() 
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Your Status Here')) # Change your status here
+    print('Connected to bot: {}'.format(bot.user.name)) # Shows that bot is connected to your token and Discord APU
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if 'Hello!' in message.content:
+        await message.channel.send('Hello there!')  # Add your responses here. 
+
+bot.run('Token Here.')  # Put your token here
